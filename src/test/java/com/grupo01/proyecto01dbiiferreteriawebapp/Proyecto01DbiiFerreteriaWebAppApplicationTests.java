32262ae2d4f9af7732002a1bd9c3db400ed27891
adam3497 <amador19972012@gmail.com>
2023-03-09 21:11:36 -0600

    Initial commit

---
+++ src/test/java/com/grupo01/proyecto01dbiiferreteriawebapp/Proyecto01DbiiFerreteriaWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo01.proyecto01dbiiferreteriawebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto01DbiiFerreteriaWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
