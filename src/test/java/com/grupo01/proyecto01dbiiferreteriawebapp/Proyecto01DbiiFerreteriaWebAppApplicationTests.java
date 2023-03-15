a100e3c8bd2570f339c4ec230a34fc95098e6cfe
adam3497 <amador19972012@gmail.com>
2023-03-14 19:39:11 -0600

    Some pom modifications

--- src/test/java/com/grupo01/proyecto01dbiiferreteriawebapp/Proyecto01DbiiFerreteriaWebAppApplicationTests.java
+++ src/test/java/com/grupo01/proyecto01dbiiferreteriawebapp/Proyecto01DbiiFerreteriaWebAppApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Proyecto01DbiiFerreteriaWebAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
